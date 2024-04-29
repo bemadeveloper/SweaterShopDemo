c4aeb20d5e2b1d9380fbb5d197799560922aa962
bemadeveloper <akbermet1990@icloud.com>
2024-04-29 13:13:00 +0600

    Initial Commit

---
+++ SweaterShopDemo/SweaterShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopDemoApp.swift
+//  SweaterShopDemo
+//
+//  Created by Bema on 29/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
